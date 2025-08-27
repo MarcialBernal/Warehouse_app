349459745a7fa545bb3402d314d839c0d3f83b28
Marcial Bernal <marcialb9328@gmail.com>
2025-08-26 21:47:21 -0700

    Modificado las funciones que heredan, poniendido atributos fuera del supper

--- frontend/src/components/sidebar.py
+++ frontend/src/components/sidebar.py
@@ -2,39 +2,42 @@ import flet as ft
 
 class SideBar(ft.NavigationRail):
     def __init__(self):
-        super().__init__(
-            selected_index=0,
-            label_type=ft.NavigationRailLabelType.ALL,
-            min_width=100,
-            min_extended_width=400,
-            leading=ft.FloatingActionButton(
-                icon=ft.Icons.CREATE, text="Add Item to Warehouse", on_click=self.fab_clicked
+        super().__init__() 
+        self.selected_index = 0
+        self.label_type = ft.NavigationRailLabelType.ALL
+        self.min_width = 100
+        self.min_extended_width = 400
+        self.group_alignment = -0.9
+
+        
+        self.destinations = [
+            ft.NavigationRailDestination(
+                icon=ft.Icons.CREATE_ROUNDED,
+                selected_icon=ft.Icons.CREATE,
+                label="Add Item to Warehouse",
             ),
-            group_alignment=-0.9,
-            destinations=[
-                ft.NavigationRailDestination(
-                    icon=ft.Icons.FAVORITE_BORDER,
-                    selected_icon=ft.Icons.FAVORITE,
-                    label="First",
-                ),
-                ft.NavigationRailDestination(
-                    icon=ft.Icons.BOOKMARK_BORDER,
-                    selected_icon=ft.Icons.BOOKMARK,
-                    label="Second",
-                ),
-                ft.NavigationRailDestination(
-                    icon=ft.Icons.SETTINGS_OUTLINED,
-                    selected_icon=ft.Icons.SETTINGS,
-                    label_content=ft.Text("Settings"),
-                ),
-            ],
-            on_change=self.on_destination_change,
-        )
+            ft.NavigationRailDestination(
+                icon=ft.Icons.FAVORITE_BORDER,
+                selected_icon=ft.Icons.FAVORITE,
+                label="Inventario",
+            ),
+            ft.NavigationRailDestination(
+                icon=ft.Icons.BOOKMARK_BORDER,
+                selected_icon=ft.Icons.BOOKMARK,
+                label="Requisiciones",
+            ),
+            ft.NavigationRailDestination(
+                icon=ft.Icons.SETTINGS_OUTLINED,
+                selected_icon=ft.Icons.SETTINGS,
+                label_content=ft.Text("Settings"),
+            ),
+        ]
+        self.on_change = self.on_destination_change
 
     def fab_clicked(self, e):
         pass
-        #print("FAB clicked!")
+        
 
     def on_destination_change(self, e):
         pass
-        #print("Selected destination:", e.control.selected_index)
+        
--- frontend/src/components/topbar.py
+++ frontend/src/components/topbar.py
@@ -2,20 +2,20 @@ import flet as ft
 
 class TopBar(ft.AppBar):
     def __init__(self):
-        # Llamamos al constructor de AppBar con los atributos oficiales
-        super().__init__(
-            leading=ft.Icon(ft.Icons.WAREHOUSE),
-            leading_width=40,
-            title=ft.Text("WAREHOUSE"),
-            center_title=False,
-            bgcolor=ft.Colors.SURFACE_CONTAINER_HIGHEST,
-            actions=[
-                ft.ElevatedButton("ADMIN / Login",
-                                    icon=ft.Icons.ADMIN_PANEL_SETTINGS,
-                                    icon_color=ft.Colors.PINK_400,),
+        super().__init__() 
+        self.leading = ft.Icon(ft.Icons.WAREHOUSE)
+        self.leading_width = 40
+        self.title = ft.Text("WAREHOUSE")
+        self.center_title = False
+        self.bgcolor = ft.Colors.SURFACE_CONTAINER_HIGHEST
 
-            ],
-        )
+        self.actions = [
+            ft.ElevatedButton(
+                "ADMIN / Login",
+                icon=ft.Icons.ADMIN_PANEL_SETTINGS,
+                icon_color=ft.Colors.PINK_400,
+            ),
+        ]
 
     # Método para manejar clicks en el PopupMenu
     def check_item_clicked(self, e):
